a57de82eec69a42367255e7ebbb352aae381ee57
Joshua Jia-Rui Soong <jjrsoong@magneton.cs.uchicago.edu>
2021-02-22 22:04:20 -0600

    State Matrix

--- scripts/q_learning.py
+++ scripts/q_learning.py
@@ -3,19 +3,49 @@
 import rospy, cv2, cv_bridge, numpy
 from sensor_msgs.msg import Image
 from geometry_msgs.msg import Twist
+from q_learning_project.msg import QMatrix, QMatrixRow, RobotMoveDBToBlock, QLearningReward
 
-class Follower:
+class QLearner:
 
         def __init__(self):
+            #Once everything is initialized will set to true
+            self.initialized = False
 
-                # set up ROS / OpenCV bridge
-                self.bridge = cv_bridge.CvBridge()
+            #set up Subscriber and Publishers
+            #TODO link correct function for Sub
+            self.q_reward = rospy.Subscriber('/q_learning/reward', QLearningReward, self.image_callback)
+            self.q_matrix = rospy.Publisher('/q_learning/q_matrix', QMatrix, queue_size=10)
+            self.bot_action = rospy.Publisher('/q_learning/robot_action', RobotMoveDBToBlock, queue_size=10)
+            print('testing')
 
-                # subscribe to the robot's RGB camera data stream
-                self.image_sub = rospy.Subscriber('camera/rgb/image_raw',
-                        Image, self.image_callback)
+            self.initialize_state_matrix()
+            print(self.state_matrix)
+            #self.initialize_action_matrix()
 
-                self.navigator = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+            self.initialized = True
+
+                # # set up ROS / OpenCV bridge
+                # self.bridge = cv_bridge.CvBridge()
+                #
+                # # subscribe to the robot's RGB camera data stream
+                # self.image_sub = rospy.Subscriber('camera/rgb/image_raw',
+                #         Image, self.image_callback)
+                #
+                # self.navigator = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+
+        # The state matrix is a dict where the key is the state number and the value is an array
+        # contain the red, green, and blue dumbell locations in that order.
+        # For reference about locations: 0 = Origin,1 = Block 1, 2 = Block 2, 3 = Block 3
+        def initialize_state_matrix(self):
+            self.state_matrix = {}
+
+            state_num = 0
+            for blue_loc in range(0, 4):
+                for green_loc in range(0, 4):
+                    for red_loc in range(0, 4):
+                        self.state_matrix[state_num] = [red_loc, green_loc, blue_loc]
+                        state_num = state_num + 1
+
 
         def image_callback(self, msg):
 
@@ -71,6 +101,7 @@ class Follower:
 
 if __name__ == '__main__':
 
-        rospy.init_node('line_follower')
-        follower = Follower()
-        follower.run()
+    #TODO Change this init_node
+        rospy.init_node('line_QLearner')
+        q_learner = QLearner()
+        q_learner.run()
